c0d2a3175f7bfcc60f87f1e0635105e2257024a7
TonyRone <ace1998116@gmail.com>
2023-03-04 14:58:01 +0300

    s

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-export const API_URL ='https://test.vocco.ai';
+export const API_URL ='https://api.vocco.ai';
 export const BIRD_URL = 'https://api-EDFFA883-5923-489F-90BD-A5C70C46A037.calls.sendbird.com/v1/rooms'
-//export const API_URL = 'http://192.168.111.196:80';
