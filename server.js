2e66b0458464e569fc78b473689a9faa6c7cf074
TonyRone <ace1998116@gmail.com>
2023-03-13 19:44:31 +0300

    s

--- server.js
+++ server.js
@@ -116,6 +116,7 @@ io.on("connection", (socket) => {
   }
 
   socket.on("login", ({ uid, email, isNew }, callback) => {
+    console.log("Login: ", uid);
     const selfIndex = vc_users.findIndex((e_user) => e_user.id === socket.id);
     if (selfIndex != -1 || (users_byId[uid] && users_byId[uid].last_seen == 'onSession')) {
       callback("Already login");
