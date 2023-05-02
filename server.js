d8b0bb80176e675c4ad74004ab9c38232fd0414b
TonyRone <ace1998116@gmail.com>
2023-05-02 16:18:59 +0300

    s

--- server.js
+++ server.js
@@ -202,6 +202,7 @@ io.on("connection", (socket) => {
         type: info.type,
         value: info.value,
         user: info.user,
+        parentId: info?.parentId,
         createdAt: new Date()
       };
       chatRooms[chatRoomIndex].messages.push(message);
